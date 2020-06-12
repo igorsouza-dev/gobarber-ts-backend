77ea2be4c341bbd6f8a0bf09c545205a2e93974f
Igor Souza <igorsouza.dev@gmail.com>
2020-06-12 16:12:14 -0300

    Added users route

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -14,14 +14,14 @@ router.get('/', async (request, response) => {
 
 router.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
     const createAppointment = new CreateAppointmentService();
 
     const appointment = await createAppointment.execute({
       date: parsedDate,
-      provider,
+      provider_id,
     });
 
     return response.json(appointment);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
@@ -8,5 +9,6 @@ routes.get('/', (request, response) =>
 );
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
