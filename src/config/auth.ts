0d0d8593ea96533f1797ddc5dd97fcc0b9679d04
Igor Souza <igorsouza.dev@gmail.com>
2020-06-15 16:34:14 -0300

    Added session creation with JWT

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'e2686b9b035c13323ff702724305ceae',
+    expiresIn: '1d',
+  },
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
+
+import authMiddleware from '../middlewares/auth';
 
 const routes = Router();
 
@@ -8,7 +11,10 @@ routes.get('/', (request, response) =>
   response.json({ message: 'Hello world' }),
 );
 
-routes.use('/appointments', appointmentsRouter);
+routes.use('/sessions', sessionsRouter);
 routes.use('/users', usersRouter);
+
+routes.use(authMiddleware);
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
